1a13b3d3639dffcc3949e9719de65e965dfb057d
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 04:06:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (typeof cardNumber === undefined) {
+  if (typeof cardNumber === 'undefined') {
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
   
