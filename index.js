a94c00992c70e178a1ba644cd8ff2c44d98d3e1e
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 03:27:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function viewCart() {
   // write your code here
   if (cart.length === 0) {
     return 'Your shopping cart is empty.'
-  } elseif (cart.length === 1) {
+  } else if (cart.length === 1) {
     return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
   }
 }
