6e8e5cfd68dc3fdfb2646f3f780f6395bb2cc762
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 03:50:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function removeFromCart(item) {
   // write your code here
   //var position = cart.indexOf(item);
   for (let i = 0; i < cart.length; i++) {
-    if (cart[i].itemName === item) {
+    if (getCart()[i].itemName === item) {
       delete cart[i];
     }
   }
