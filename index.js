ef411a39be54bce720fdc00fdec2ec7337e3ac1c
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 04:05:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -57,4 +57,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if (cardNumber.length === 0) {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  }
+  
 }
