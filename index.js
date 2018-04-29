1d789f0bc9a5837e2f20ade8dd09db2106512f8e
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 03:52:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  //var position = cart.indexOf(item);
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       delete cart[i];
