a1eccdd559906c65f326044454657c25cec87339
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 03:33:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   } else if (cart.length === 2) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+  } else {
+    for (let i = 0; i < cart.length; i++) {
+      var nextItem
+      var print ==
+    }
   }
 }
 
